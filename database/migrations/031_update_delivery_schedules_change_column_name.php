5cb19b76306155e73a9fb4df4366ba5ba4ccb461
Indra-haha <124240180@student.upnyk.ac.id>
2026-06-13 23:00:56 +0700

    build: change column in delivery schedules

---
+++ database/migrations/031_update_delivery_schedules_change_column_name.php
@@ -0,0 +1,22 @@
+<?php
+
+return new class {
+
+    public function up(PDO $db): void
+    {
+        $db->exec("
+            ALTER TABLE delivery_schedules
+            DROP COLUMN signature_base64,
+            ADD COLUMN signature_path VARCHAR(255) NULL AFTER status
+        ");
+    }
+
+    public function down(PDO $db): void
+    {
+        $db->exec("
+            ALTER TABLE delivery_schedules
+            DROP COLUMN signature_path,
+            ADD COLUMN signature_base64 LONGTEXT NULL
+        ");
+    }
+};
