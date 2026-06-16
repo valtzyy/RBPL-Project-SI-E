da9873e4d0388d770d557fbf397759dcd597018b
Indra-haha <124240180@student.upnyk.ac.id>
2026-06-16 14:55:14 +0700

    docs: make it easy to read

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ $router->get('/users/:id',     'UserController@show');
 $router->post('/users',        'UserController@store');
 $router->post('/users/:id',    'UserController@update');
 $router->post('/users/delete', 'UserController@destroy');
+
+// sprint-12 pembayaran services
 $router->get('/service-billing',      'ServiceBillingController@index');
-$router->get('/service-billing/:id',  'ServiceBillingController@detail');
-$router->get('/service-billing/plate/:plateNumber', 'ServiceBillingController@findByPlateNumber');
+$router->get('/service-billing/:plateNumber', 'ServiceBillingController@findByPlateNumber');
+$router->get('/service-billing/detail/:plateNumber',  'ServiceBillingController@detail');
+$router->get('/service-billing/detail/history/:plateNumber',  'ServiceBillingController@detailLog');
+
 $router->get('/kasir/dashboard', 'KasirController@dashboard');
 $router->get('/kasir/nota',      'KasirController@nota');
 $router->get('/kasir/riwayat',   'KasirController@riwayat');
