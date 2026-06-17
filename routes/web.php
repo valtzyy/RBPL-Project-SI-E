8d96998d8e209fd630441adb82412e0d022def49
Inori Hikari <mandepinto@gmail.com>
2026-06-17 10:20:48 +0700

    Router

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@
 
 // Halaman Utama
 $router->get('/',           'HomeController@index');
+$router->get('/sprint15',   'ReportController@testingPage');
 
 // Users - CRUD
 $router->get('/users',         'UserController@index');
@@ -13,3 +14,9 @@ $router->get('/users/:id',     'UserController@show');
 $router->post('/users',        'UserController@store');
 $router->post('/users/:id',    'UserController@update');
 $router->post('/users/delete', 'UserController@destroy');
+
+// Sprint 15 - Reporting, Export, Audit Log
+$router->get('/api/reports/:type', 'ReportController@report');
+$router->get('/api/reports/:type/export/pdf', 'ReportController@exportPdf');
+$router->get('/api/reports/:type/export/excel', 'ReportController@exportExcel');
+$router->get('/api/audit-logs', 'ReportController@auditLogs');
