bb2333b5a364875b213776a13e6e07ec58d90f3d
Lucy naibaho <lucynaibah@gmail.com>
2026-06-23 22:18:37 +0700

    perbaiki css

--- app/views/credit_history/histori_kelengkapan.php
+++ app/views/credit_history/histori_kelengkapan.php
@@ -83,7 +83,7 @@ if (!isset($profile, $applications, $selected, $stats, $customerId)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Histori Kredit - <?= htmlspecialchars($profile['nama']) ?> | DealerLink</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 </head>
 <body>
