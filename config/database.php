f3741e351e382e67c9dfa9a445e1ef5838411ff3
Indra-haha <124240180@student.upnyk.ac.id>
2026-07-03 01:44:53 +0700

    test

--- config/database.php
+++ config/database.php
@@ -6,12 +6,17 @@ return [
     'dbname'   => envRequired('DB_NAME'),
     'username' => envRequired('DB_USER'),
     'password' => envRequired('DB_PASS'),
+    // Biarkan bawaan jika di lokal, tapi nanti di Vercel kita bypass lewat 'options'
     'sslcert'  => env('DB_SSL_CA', dirname(__DIR__) . '/config/certs/ca.pem'),
     'charset'  => 'utf8mb4',
 
     'options'  => [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        
+        // Tambahkan 2 baris ini di dalam array options untuk Aiven di Vercel:
+        PDO::MYSQL_ATTR_SSL_CA             => '',
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
     ],
 ];
